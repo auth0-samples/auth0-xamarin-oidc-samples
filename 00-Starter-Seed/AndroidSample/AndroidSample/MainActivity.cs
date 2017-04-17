78e336e6c28f06c63f5737a94955b266ff21731f
Jerrie Pelser <jerrie@jerriepelser.com>
2017-04-17 15:45:36 +0700

    Add Android sample

---
+++ 00-Starter-Seed/AndroidSample/AndroidSample/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Auth0.OidcClient;
+
+namespace AndroidSample
+{
+    [Activity(Label = "AndroidSample", MainLauncher = true, Icon = "@drawable/icon",
+        LaunchMode = LaunchMode.SingleTask)]
+    public class MainActivity : Activity
+    {
+        private Auth0Client _client;
+        private Button _loginButton;
+        private TextView _userDetailsTextView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            _loginButton = FindViewById<Button>(Resource.Id.LoginButton);
+            _userDetailsTextView = FindViewById<TextView>(Resource.Id.UserDetailsTextView);
+
+            _client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = Resources.GetString(Resource.String.auth0_domain),
+                ClientId = Resources.GetString(Resource.String.auth0_client_id),
+                Activity = this
+            });
+
+            _loginButton.Click += LoginButtonOnClick;
+            _userDetailsTextView.Text = String.Empty;
+        }
+
+        private async void LoginButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            var loginResult = await _client.LoginAsync();
+
+            var sb = new StringBuilder();
+            if (loginResult.IsError)
+            {
+                sb.AppendLine($"An error occurred during login: {loginResult.Error}");
+            }
+            else
+            {
+                sb.AppendLine($"ID Token: {loginResult.IdentityToken}");
+                sb.AppendLine($"Access Token: {loginResult.AccessToken}");
+                sb.AppendLine($"Refresh Token: {loginResult.RefreshToken}");
+
+                sb.AppendLine();
+
+                sb.AppendLine("-- Claims --");
+                foreach (var claim in loginResult.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type} = {claim.Value}");
+                }
+            }
+
+            _userDetailsTextView.Text = sb.ToString();
+        }
+    }
+}
