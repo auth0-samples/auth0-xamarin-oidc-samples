ec40a794d09d5413b4545eb74804973c48b5f0d1
Jerrie Pelser <jerrie@jerriepelser.com>
2017-05-16 10:26:58 +0700

    Unify Xamarin samples

---
+++ Quickstart/iOS/01-Login/iOSSample/iOSSample/MyViewController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using UIKit;
+using Auth0.OidcClient;
+using System.Text;
+
+namespace iOSSample
+{
+	public partial class MyViewController : UIViewController
+	{
+		private Auth0Client _client;
+
+		public MyViewController() : base("MyViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			UserDetailsTextView.Text = String.Empty;
+
+			LoginButton.TouchUpInside += LoginButton_TouchUpInside;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		private async void LoginButton_TouchUpInside(object sender, EventArgs e)
+		{
+			_client = new Auth0Client(new Auth0ClientOptions
+			{
+				Domain = "{DOMAIN}",
+				ClientId = "{CLIENT_ID}",
+				Controller = this
+			});
+
+			var loginResult = await _client.LoginAsync(null);
+
+			var sb = new StringBuilder();
+
+			if (loginResult.IsError)
+			{
+				sb.AppendLine("An error occurred during login:");
+				sb.AppendLine(loginResult.Error);
+			}
+			else
+			{
+				sb.AppendLine($"ID Token: {loginResult.IdentityToken}");
+				sb.AppendLine($"Access Token: {loginResult.AccessToken}");
+				sb.AppendLine($"Refresh Token: {loginResult.RefreshToken}");
+				sb.AppendLine();
+				sb.AppendLine("-- Claims --");
+				foreach (var claim in loginResult.User.Claims)
+				{
+					sb.AppendLine($"{claim.Type} = {claim.Value}");
+				}
+			}
+
+			UserDetailsTextView.Text = sb.ToString();
+		}
+	}
+}
+
---
+++ Quickstart/iOS/01-Login/iOSSample/iOSSample/MyViewController.designer.cs
@@ -0,0 +1,38 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace iOSSample
+{
+    [Register ("MyViewController")]
+    partial class MyViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView UserDetailsTextView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+
+            if (UserDetailsTextView != null) {
+                UserDetailsTextView.Dispose ();
+                UserDetailsTextView = null;
+            }
+        }
+    }
+}
