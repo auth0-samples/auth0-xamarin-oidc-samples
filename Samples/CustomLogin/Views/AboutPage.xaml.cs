0008b170ff90586435d7468e65d03d85dbbb1620
Jerrie Pelser <jerrie@jerriepelser.com>
2018-01-29 09:51:05 +0700

    Add base Xamarin project for Custom Login

---
+++ Samples/CustomLogin/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CustomLogin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
