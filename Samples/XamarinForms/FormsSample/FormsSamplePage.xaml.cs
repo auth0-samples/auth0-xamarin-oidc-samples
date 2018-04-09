54e52d37fe99866df8f8b17ed58837dbc4ec5534
Jerrie Pelser <jerrie@jerriepelser.com>
2018-04-09 11:55:16 +0700

    Base Xamarin Forms project

---
+++ Samples/XamarinForms/FormsSample/FormsSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FormsSample
+{
+    public partial class FormsSamplePage : ContentPage
+    {
+        public FormsSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
