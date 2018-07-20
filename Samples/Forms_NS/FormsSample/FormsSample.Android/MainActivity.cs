f35666968899c40f01b242aac39289cfa8c79115
Jerrie Pelser <jerrie@jerriepelser.com>
2018-07-20 14:33:17 +0700

    Add .NET Standard sample

---
+++ Samples/Forms_NS/FormsSample/FormsSample.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Auth0.OidcClient;
+
+namespace FormsSample.Droid
+{
+    [Activity(Label = "FormsSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTask)]
+    [IntentFilter(
+        new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = "com.auth0.formssample",
+        DataHost = "jerrie.auth0.com",
+        DataPathPrefix = "/android/com.auth0.formssample/callback")]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            ActivityMediator.Instance.Send(intent.DataString);
+        }
+    }
+}
+
---
+++ Samples/Forms_NS/FormsSample/FormsSample/AuthenticationConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace FormsSample
+{
+    public static class AuthenticationConfig
+    {
+        public const string ClientId = "{CLIENT_ID}";
+        public const string Domain = "{DOMAIN}";
+    }
+}
