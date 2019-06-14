89b459ba3a7e01e54fb8c7d1f33b7bf2543acff6
Damien Guard <damieng@gmail.com>
2019-06-14 15:29:16 -0700

    Update Android project to latest Xamarin libs and SDK reqs

--- Samples/Forms/FormsSample/FormsSample.Android/Resources/Resource.designer.cs
+++ Samples/Forms/FormsSample/FormsSample.Android/Resources/Resource.designer.cs
@@ -3640,7 +3640,10 @@ namespace FormsSample.Droid
 			public const int icon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int launcher_foreground = 2130903041;
+			public const int icon_round = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int launcher_foreground = 2130903042;
 			
 			static Mipmap()
 			{
