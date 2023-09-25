80d441e2ff0cdfb32bd2ca6666c393b72c887975
frederikprijck <frederik.prijck@auth0.com>
2023-09-25 21:09:16 +0200

    Remove Xamarin Forms Samples

--- Samples/Forms_NS/FormsSample/FormsSample/AuthenticationConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FormsSample
-{
-    public static class AuthenticationConfig
-    {
-        public const string ClientId = "{CLIENT_ID}";
-        public const string Domain = "{DOMAIN}";
-    }
-}
